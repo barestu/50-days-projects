2854a618f2d11b0b9fc88e487a147b390e48d26b
Fandy Barestu <fandy@softwareseni.com>
2020-12-09 21:49:35 +0700

    Done day 10

---
+++ 10-dad-jokes/script.js
@@ -0,0 +1,35 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
+
+// USING ASYNC AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+
+  jokeEl.innerText = data.joke;
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(response => response.json())
+//     .then(data => {
+//       jokeEl.innerText = data.joke;
+//     });
+// }
